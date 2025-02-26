b8fbb2fa6eb577619560f44661ed4c4bb85474e5
Aminul <aminul@suffixit.com>
2025-02-26 13:16:28 +0600

    First push of grafana k6 test scripts

---
+++ stresstest.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '1m', target: 200 },
+    { duration: '2m', target: 200 },
+    { duration: '1m', target: 0 },
+  ]
+}
+
+export default function () {
+  http.get('https://test.k6.io');
+  sleep(1);
+}
